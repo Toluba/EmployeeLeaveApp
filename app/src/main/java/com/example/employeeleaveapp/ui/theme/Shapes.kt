7e2ffd6b4cc0f34418e09cc9ac09ac7ddd50073d
Toluba <tomikeolubamowo@aol.com>
2023-10-23 20:10:10 +0100

    created a reusable circle shape

---
+++ app/src/main/java/com/example/employeeleaveapp/ui/theme/Shapes.kt
@@ -0,0 +1,50 @@
+package com.example.employeeleaveapp.ui.theme
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun Circle(
+    color: Color,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Box(
+            modifier
+                .size(15.dp)
+                .clip(shape)
+                .background(color, shape = CircleShape)
+                .padding(5.dp),
+        )
+    }
+}
+
+
+
+@Composable
+fun CircleShapeDemo() {
+    //ExampleBox(CircleShape,)
+}
+
+@Preview
+@Composable
+fun CirclePreview() {
+    Circle(Pink40)
+}
