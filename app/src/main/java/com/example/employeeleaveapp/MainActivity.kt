20899bed9e3f7b6ff90725386a9c341881921ce3
Toluba <tomikeolubamowo@aol.com>
2023-10-24 09:59:44 +0100

    topbar wip

--- app/src/main/java/com/example/employeeleaveapp/MainActivity.kt
+++ app/src/main/java/com/example/employeeleaveapp/MainActivity.kt
@@ -5,25 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import com.example.employeeleaveapp.BottomNav.NavBar
 import com.example.employeeleaveapp.ui.theme.EmployeeLeaveAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,57 +33,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-val bottomNavItems = listOf(
-    BottomNavItem(
-        title = "Home",
-        route = "home",
-        icon = Icons.Filled.Home
-    ),
-    BottomNavItem(
-        title = "Requests",
-        route = "requests",
-        icon = Icons.Filled.Create
-    ),
-    BottomNavItem(
-        title = "Calender",
-        route = "teamcalendar",
-        icon = Icons.Filled.DateRange,
-    ),
-    BottomNavItem(
-        title = "Settings",
-        route = "settings",
-        icon = Icons.Filled.Settings,
-    ),
-)
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NavBar() {
-
-    var selectedItem by remember { mutableIntStateOf(0) }
-
-    NavigationBar {
-        bottomNavItems.forEachIndexed { index, item ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = "${item.title} Icon"
-                    )
-                },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index },
-                label = { Text(text = item.title) },
-            )
-        }
-    }
-}
-
-
-
-data class BottomNavItem(var title: String, var icon: ImageVector, var route: String)
-
-@Preview(showBackground = true)
-@Composable
-fun NavPreview() {
-
+fun topBar() {
+    TopAppBar(title = {
+        Text(
+            text = "My Important Dates",
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center
+        )
+    })
 }
