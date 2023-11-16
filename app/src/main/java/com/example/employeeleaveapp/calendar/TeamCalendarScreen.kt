fdaff5a0009a5b9d4438a2b8a15b58115aacce27
Toluba <tomikeolubamowo@aol.com>
2023-11-16 17:39:30 +0000

    Calender ui implemmented wip

--- app/src/main/java/com/example/employeeleaveapp/calendar/TeamCalendarScreen.kt
+++ app/src/main/java/com/example/employeeleaveapp/calendar/TeamCalendarScreen.kt
@@ -1,70 +1,223 @@
 package com.example.employeeleaveapp.calendar
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.employeeleaveapp.ui.theme.Pink40
-import kotlinx.coroutines.launch
-import java.time.DayOfWeek
-import java.time.YearMonth
-import java.util.Locale
+
+import com.waseefakhtar.doseapp.extension.toFormattedDateShortString
+import com.waseefakhtar.doseapp.extension.toFormattedDateString
+import com.waseefakhtar.doseapp.extension.toFormattedMonthDateString
+import com.waseefakhtar.doseapp.feature.home.data.CalendarDataSource
+import com.waseefakhtar.doseapp.feature.home.model.CalendarModel
+import java.util.Calendar
+import java.util.Date
+
 
 @Composable
 fun CalendarScreen(){
-    Box(modifier = Modifier.background(color = Pink40)){
-        Text(
-            text = "CalendarScreen",
-            style = MaterialTheme.typography.displayLarge
+    Surface(){
+       DatesHeader(onDateSelected ={it})
+
+    }
+}
+
+
+@Composable
+fun DatesHeader(
+    onDateSelected: (CalendarModel.DateModel) -> Unit // Callback to pass the selected date
+) {
+    val dataSource = CalendarDataSource()
+    var calendarModel by remember { mutableStateOf(dataSource.getData(lastSelectedDate = dataSource.today)) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        DateHeader(
+            data = calendarModel,
+            onPrevClickListener = { startDate ->
+                // refresh the CalendarModel with new data
+                // by get data with new Start Date (which is the startDate-1 from the visibleDates)
+                val calendar = Calendar.getInstance()
+                calendar.time = startDate
+
+                calendar.add(Calendar.DAY_OF_YEAR, -2) // Subtract one day from startDate
+                val finalStartDate = calendar.time
+
+                calendarModel = dataSource.getData(startDate = finalStartDate, lastSelectedDate = calendarModel.selectedDate.date)
+            },
+            onNextClickListener = { endDate ->
+                // refresh the CalendarModel with new data
+                // by get data with new Start Date (which is the endDate+2 from the visibleDates)
+                val calendar = Calendar.getInstance()
+                calendar.time = endDate
+
+                calendar.add(Calendar.DAY_OF_YEAR, 2)
+                val finalStartDate = calendar.time
+
+                calendarModel = dataSource.getData(startDate = finalStartDate, lastSelectedDate = calendarModel.selectedDate.date)
+            }
+        )
+        DateList(
+            data = calendarModel,
+            onDateClickListener = { date ->
+                calendarModel = calendarModel.copy(
+                    selectedDate = date,
+                    visibleDates = calendarModel.visibleDates.map {
+                        it.copy(
+                            isSelected = it.date.toFormattedDateString() == date.date.toFormattedDateString()
+                        )
+                    }
+                )
+                onDateSelected(date)
+            }
         )
     }
 }
 
-private object Example3RippleTheme : RippleTheme {
-    @Composable
-    override fun defaultColor() = RippleTheme.defaultRippleColor(Color.Gray, lightTheme = true)
-
-    @Composable
-    override fun rippleAlpha() = RippleTheme.defaultRippleAlpha(Color.Gray, lightTheme = true)
+@Composable
+fun DateList(
+    data: CalendarModel,
+    onDateClickListener: (CalendarModel.DateModel) -> Unit
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        items(items = data.visibleDates) { date ->
+            DateItem(date, onDateClickListener)
+        }
+    }
 }
 
-@Preview(heightDp = 600)
+@Composable
+fun DateItem(
+    date: CalendarModel.DateModel,
+    onClickListener: (CalendarModel.DateModel) -> Unit,
+) {
+    Column {
+        Text(
+            text = date.day, // day "Mon", "Tue"
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Normal,
+            color = MaterialTheme.colorScheme.outline
+        )
+        Card(
+            modifier = Modifier
+                .padding(vertical = 4.dp, horizontal = 4.dp)
+                .clickable { onClickListener(date) },
+            colors = CardDefaults.cardColors(
+                // background colors of the selected date
+                // and the non-selected date are different
+                containerColor = if (date.isSelected) {
+                    MaterialTheme.colorScheme.tertiary
+                } else {
+                    MaterialTheme.colorScheme.surface
+                }
+            ),
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(42.dp)
+                    .height(42.dp)
+                    .padding(8.dp)
+                    .fillMaxSize(), // Fill the available size in the Column
+                verticalArrangement = Arrangement.Center, // Center vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+            ) {
+                Text(
+                    text = date.date.toFormattedDateShortString(),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = if (date.isSelected) {
+                        FontWeight.Medium
+                    } else {
+                        FontWeight.Normal
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DateHeader(
+    data: CalendarModel,
+    onPrevClickListener: (Date) -> Unit,
+    onNextClickListener: (Date) -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(vertical = 16.dp),
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically),
+            text = if (data.selectedDate.isToday) {
+                "Today"
+            } else {
+                data.selectedDate.date.toFormattedMonthDateString()
+            },
+            style = MaterialTheme.typography.displaySmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.tertiary
+        )
+        IconButton(onClick = {
+            onPrevClickListener(data.startDate.date)
+        }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                tint = MaterialTheme.colorScheme.tertiary,
+                contentDescription = "Back"
+            )
+        }
+        IconButton(onClick = {
+            onNextClickListener(data.endDate.date)
+        }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                tint = MaterialTheme.colorScheme.tertiary,
+                contentDescription = "Next"
+            )
+        }
+    }
+}
+
+@Preview()
 @Composable
 private fun Example3Preview() {
-   // Example3Page()
+    CalendarScreen()
+
 }
